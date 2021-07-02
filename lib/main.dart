0dc857972ef6c64e38a49585894ce52c82c29b5f
dipkrkapat <dip.eduapp@gmail.com>
2021-07-02 08:37:22 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Container(
-          child: Center(child: Text("Welcome to Catalog App")),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
